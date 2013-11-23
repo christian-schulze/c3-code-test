069653616b258a584879b5387d1e5b31410870d2
Christian Schulze <christian.schulze@gmail.com>
2013-11-23 21:52:18 +1100

    output messages

--- app.rb
+++ app.rb
@@ -5,5 +5,6 @@ require_relative 'lib/c3_code_test'
 c3 = C3CodeTest.new
 
 ARGV.each do |file_path|
-  c3.process_file(file_path)
+  messages = c3.process_file(file_path)
+  messages.each { |message| puts message }
 end
