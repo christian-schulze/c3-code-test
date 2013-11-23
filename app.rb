9b37118e981318b2be60d13e98f140ba78d5e853
Christian Schulze <christian.schulze@gmail.com>
2013-11-23 21:55:15 +1100

    show user what file is being processed

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ require_relative 'lib/c3_code_test'
 c3 = C3CodeTest.new
 
 ARGV.each do |file_path|
+  puts "Processing '#{file_path}':"
+
   messages = c3.process_file(file_path)
   messages.each { |message| puts message }
+
+  puts "Completed."
 end
